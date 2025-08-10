5d4fd5374c172e0c8319f994f4b48d4a805ea475
Shahriar Anan <shahriaranany6@gmail.com>
2025-08-11 03:04:48 +0600

    Initial Product repository and services added

---
+++ src/main/java/com/ecommerce/repository/ProductRepository.java
@@ -0,0 +1,23 @@
+package com.ecommerce.repository;
+
+import com.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+@Transactional
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+    Product findByName(String name);
+    List<Product> findByNameContainingIgnoreCase(String category);
+    List<Product> findByPriceLessThanEqual(double price);
+    List<Product> findByPriceGreaterThanEqual(double price);
+    List<Product> findByPriceBetween(double low, double high);
+    List<Product> findByCategory(String category);
+    List<Product> findByRatingAndNameIgnoreCaseOrderByRatingAsc(double rating, String name);
+    List<Product> findByStockQuantityGreaterThanEqual(int stockQuantity);
+    List<Product> findByStockQuantityLessThanEqual(int stockQuantity);
+}
