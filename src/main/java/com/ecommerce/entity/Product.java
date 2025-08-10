ec6a7ba182b5229cb415ca9be31a0dcec281b080
Shahriar Anan <shahriaranany6@gmail.com>
2025-08-11 02:48:04 +0600

    Basic Product Entity for CRUD testing

---
+++ src/main/java/com/ecommerce/entity/Product.java
@@ -0,0 +1,46 @@
+package com.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="products")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(length = 500)
+    private String description;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(nullable = false)
+    private Integer stockQuantity;
+
+    @Column(nullable = false, length = 100)
+    private String category;
+
+    @Column()
+    private double rating;
+
+    @Column(name="created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name="updated_at")
+    private LocalDateTime updatedAt;
+}
